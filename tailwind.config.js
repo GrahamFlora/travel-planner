4123969b53016411790eaa7bacb0d88b2463c3e0
Graham Flora <grahamrossflora@gmail.com>
2025-11-25 20:31:20 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (MUST BE AT THE ROOT)
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+}
